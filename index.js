65aa22773f5d667adff80e6096f6fc724343a083
Dima Fedotov <1990bas@gmail.com>
2017-02-01 17:28:06 +0200

    rework config
    feature-7928

--- index.js
+++ index.js
@@ -9,8 +9,9 @@
 
 
 var Modal     = require('mag-component-modal'),
+    List      = require('mag-component-list'),
     CheckList = require('mag-component-check-list'),
-    CheckBox  = require('spa-component-checkbox');
+    dom       = require('spa-dom');
 
 
 /**
@@ -26,6 +27,7 @@ var Modal     = require('mag-component-modal'),
  * @param {Object} [config.visible] visibility flag
  * @param {Object} [config.children] content (inherited from the parent)
  * @param {Object} [config.list] all init parameters for mag-component-check-list constructor
+ * @param {Object} [config.labelIcon] left panel list item icon
  *
  * @example
  * var modalCheckList = new ModalCheckList({
@@ -35,11 +37,7 @@ var Modal     = require('mag-component-modal'),
  *             page.panelSet.focus();
  *         }
  *     },
- *     labels:{
- *         icon:a,
- *         text:b,
- *         count:c
- *     },
+ *     labelIcon: 'theme-icon-filter',
  *     list: {
  *         size: 2,
  *         data: [
@@ -50,143 +48,132 @@ var Modal     = require('mag-component-modal'),
  *     }
  * });
  * page.add(modalCheckList);
+ *
+ * leftPanel.add(leftPanelList = new LayoutList({
+ *     size: 6,
+ *     data: [
+ *         {
+ *             items: page.modalCheckList.getListItem(),
+ *             click: function () {
+ *                 panelSet.blur();
+ *                 modalCheckList.show();
+ *             }
+ *         }
+ *     ]
+ * }));
+ *
  */
 
 
 function ModalCheckList ( config ) {
     var self = this;
 
+    if ( DEVELOP ) {
+        if ( typeof config !== 'object' ) {
+            throw new Error(__filename + ': wrong config type');
+        }
+        if ( !config.list ) {
+            throw new Error(__filename + ': wrong or empty mag-component-check-list component config config.list');
+        }
+    }
+
     // sanitize
     config = config || {};
     config.list = config.list || {};
-    config.list.render = config.list.render || renderItem;
     config.list.events = config.list.events || {};
     config.children = config.children || [];
-    config.labels = config.labels || {};
 
-    if ( DEVELOP ) {
-//        if ( typeof config !== 'object' ) {
-//            throw new Error(__filename + ': wrong config type');
-//        }
-//        // init parameters checks
-//        if ( config.icon && typeof config.icon !== 'string' ) {
-//            throw new Error(__filename + ': wrong or empty config.icon');
-//        }
-//        if ( config.title && typeof config.title !== 'string' ) {
-//            throw new Error(__filename + ': wrong or empty config.title');
-//        }
-//        if ( 'className' in config && (!config.className || typeof config.className !== 'string') ) {
-//            throw new Error(__filename + ': wrong or empty config.className');
-//        }
-//        if ( config.$body ) {
-//            throw new Error(__filename + ': config.$body should not be provided in Modal manually');
-//        }
+    this.label = {};
+    if ( config.labelIcon ) {
+        this.label.$icon = dom.tag('div', {className: 'theme-icon ' + config.labelIcon});
+    }
+    this.label.$text = dom.tag('div', {className: 'leftListItemText'});
+    this.label.$count = dom.tag('div', {className: 'theme-icon'},
+        dom.tag('div', {className: 'theme-counter'},
+            dom.tag('div')
+        )
+    );
+    this.label.$count.style.visibility = 'hidden';
+    // set title to left panel item
+    if ( config.list.data && config.list.data.length && config.list.data[0].title ) {
+        this.label.$text.innerText = config.list.data[0].title;
     }
 
-    this.labels = {
-        icon: config.labels.icon,
-        text: config.labels.text,
-        count: config.labels.count
-    };
-
     this.list = new CheckList(config.list);
+
+    this.list.setData = function ( config ) {
+        List.prototype.setData.call(self.list, config);
+        if ( self.$titleCount ) {
+            self.$titleCount.innerText = config.data.length ? config.data.length - 1 : 0;
+        }
+    };
     config.children.push(this.list);
 
 
     // parent constructor call
     Modal.call(this, config);
 
+    self.$header.appendChild(dom.tag('div', {className: 'theme-icon'},
+        dom.tag('div', {className: 'theme-counter'},
+            self.$titleCount = dom.tag('div', {},
+                config.list.data.length ? config.list.data.length - 1 : 0
+            )
+        )
+    ));
+
     // rewrite onclick listener
     this.list.events['click:item'] = undefined;
     this.list.addListener('click:item', function ( event ) {
         var item   = event.$item,
             marked = [],
-            data   = self.list.data,
-            title, count, i;
+            data   = self.list.data || [];
 
         item.checkBox.set(!item.checkBox.value);
         item.state = item.checkBox.value;
         data[item.index].state = item.checkBox.value;
 
         // go through list to collect selected items
-        for ( i = 0; i < data.length; i++ ) {
-            console.log('i: ' + i + ' ?: ' + data[i].state);
+        for ( var i = 0; i < data.length; i++ ) {
+            //console.log('i: ' + i + ' ?: ' + data[i].state);
             if ( data[i].state ) {
                 marked.push(data[i]);
             }
         }
 
-        // 1) if none was set, set "All_items" as selected
-        // 2) if "All_items" was set remove from others
-        // 3) if all except "All_items" was selected remove from others and set to "All_items"
         if (
-            !marked.length ||
-            marked.indexOf(data[0]) !== -1 && marked.length > 1 && item.index === 0 ||
-            marked.indexOf(data[0]) === -1 && marked.length === data.length - 1
+            !marked.length || // if none set "All" as selected
+            marked.indexOf(data[0]) !== -1 && marked.length > 1 && item.index === 0 || // if "All" was set (reset others)
+            marked.indexOf(data[0]) === -1 && marked.length === data.length - 1  // if all except "All"  was selected
         ) {
-            console.log('case 1 : set to "all"');
+            //console.log('case 1 : set to "all"');
             for ( i = 0; i < data.length; i++ ) {
                 data[i].state = i === 0; // mark only first
             }
             this.setData({data: data, focusIndex: this.$focusItem.index});
-            self.labels.text.innerText = data[0].title;
-            self.labels.icon.classList.remove('active');
-            self.labels.count.innerText = 0;
+            self.label.$text.innerText = data[0].title;
+            self.label.$icon.classList.remove('active');
+            self.label.$count.style.visibility = 'hidden';
             return;
         }
-        //  if item and "All_items" item was set remove from "All_items"
+        //  if "All" item selected (set to "all" and remove from others)
         if ( marked.indexOf(data[0]) !== -1 && marked.length > 1 && item.index !== 0 ) {
-            console.log('case 2 : set to not "all"');
+            //console.log('case 2 : set to not "all"');
             data[0].state = false;
             this.setData({data: data, focusIndex: this.$focusItem.index});
-            self.labels.count.innerText = marked.length - 1;
-            self.labels.text.innerText = marked[1].title + (marked[2] ? ', ' + marked[2].title : '');
-            self.labels.icon.classList.add('active');
+            self.label.$count.style.visibility = 'inherit';
+            self.label.$count.firstChild.firstChild.innerText = marked.length - 1;
+            self.label.$text.innerText = marked[1].title + (marked[2] ? ', ' + marked[2].title : '');
+            self.label.$icon.classList.add('active');
             return;
         }
-        // renew selected items info
         if ( marked.length ) {
-            console.log('case 3 : set');
-            self.labels.count.innerText = marked.length;
-            self.labels.text.innerText = marked[0].title + (marked[1] ? ', ' + marked[1].title : '');
-            self.labels.icon.classList.add('active');
+            //console.log('case 3 : set');
+            self.label.$count.style.visibility = 'inherit';
+            self.label.$count.firstChild.firstChild.innerText = marked.length;
+            self.label.$text.innerText = marked[0].title + (marked[1] ? ', ' + marked[1].title : '');
+            self.label.$icon.classList.add('active');
         }
     });
-
-    function renderItem ( $item, data ) {
-        var table   = document.createElement('table'),
-            tr      = document.createElement('tr'),
-            td      = document.createElement('td'),
-            wrapper = document.createElement('div'),
-            check   = new CheckBox({
-                value: data.state || false
-            });
-
-        if ( this.data[0] === $item.data ) { // set underline to first item
-            wrapper.classList.add('theme-header');
-        }
-
-        $item.innerHTML = '';
-
-        table.appendChild(tr);
-
-        td.appendChild(check.$node);
-        td.className = 'checkBoxWrapper';
-        tr.appendChild(td);
-
-        td = document.createElement('td');
-        td.className = 'title';
-        td.innerText = data.title || '';
-        tr.appendChild(td);
-
-        $item.checkBox = check;
-
-        $item.state = check.value;
-        $item.value = data.value;
-
-        wrapper.appendChild(table);
-        $item.appendChild(wrapper);
-    }
 }
 
 
@@ -196,6 +183,23 @@ ModalCheckList.prototype.constructor = ModalCheckList;
 
 // set component name
 ModalCheckList.prototype.name = 'mag-component-modal mag-component-modal-check-list';
+
+/**
+ * Get list of dom elements for use in right panel list
+ * @return {Array} elements prepared for right panel layout list
+ */
+ModalCheckList.prototype.getListItem = function () {
+    var elements = [];
+
+    if ( this.label.$icon ) {
+        elements.push(this.label.$icon);
+    }
+
+    elements.push(this.label.$text);
+    elements.push(this.label.$count);
+
+    return elements;
+};
 
 
 // public
